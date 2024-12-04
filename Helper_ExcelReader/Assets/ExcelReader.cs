96e3a003a06187dd5a61d9f21828c76ef5a1f4e0
重剑无锋 <48747051+nearGale@users.noreply.github.com>
2024-12-05 00:11:03 +0800

    实现表格读取

---
+++ Helper_ExcelReader/Assets/ExcelReader.cs
@@ -0,0 +1,27 @@
+using ExcelDataReader;
+using System.IO;
+using UnityEngine;
+ 
+public class ExcelReader : MonoBehaviour
+{
+    void Start()
+    {
+        using (var stream = File.Open(Application.dataPath + @"/Excels/神奇宝贝.xlsx", FileMode.Open, FileAccess.Read))
+        {
+            using (var reader = ExcelReaderFactory.CreateReader(stream))
+            {
+                var result = reader.AsDataSet();
+                if (result.Tables.Count > 0)
+                {
+                    for (int i = 0; i < result.Tables[0].Rows.Count; i++)
+                    {
+                        for (int j = 0; j < result.Tables[0].Columns.Count; j++)
+                        {
+                            Debug.Log(result.Tables[0].Rows[i][j].ToString());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity_Helper_ExcelReader
+ Unity Excel 读取工具
